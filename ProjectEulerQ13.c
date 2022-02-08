f18d0e955b9c0021cbe50fd09618a4b18efe8c84
Lucas Weber <lweber07@uoguelph.ca>
2022-02-07 19:46:23 -0500

    currently trying to manually add digits; first the ones column, then the tens column, etc

--- ProjectEulerQ13.c
+++ ProjectEulerQ13.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
-#define DIGITS 10
+#define GOALDIGITS 10
 #define NUMS 100
+#define TOTALDIGITS 50
+#define SUMDIGITS 10000
 
-// long long still isnt large enough need to create more efficient algorithm
 int main(void)
 {
     FILE * fptr = fopen("test.txt", "r");
-    long long int num[NUMS];
-    //long long int sum = 0;
+    int num[NUMS][TOTALDIGITS];
+    int sum[SUMDIGITS];
+    int counter = 0;
 
     for (int i = 0; i < NUMS; i++)
     {
-        fscanf(fptr, "%lld", &num[i]);
+        for (int j = 0; j < TOTALDIGITS; j++)
+        {
+            fscanf(fptr, "%1d", &num[i][j]);
+        }
     }
-    
+
+    /*
     for (int i = 0; i < NUMS; i++)
     {
-        printf("%lld\n", num[i]);
+        for (int j = 0; j < TOTALDIGITS; j++)
+        {
+            printf("%d", num[i][j]);
+        }
+        printf("\n");
+    }
+    */
+
+   // trying to manually add digits first the ones column then the tens column etc
+
+    for (int i = TOTALDIGITS - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < NUMS; j++)
+        {
+            sum[counter] += num[j][i];
+        }
     }
 
     return 0;
